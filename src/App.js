86ab6b7dc647f7e43673b27c7367adb7174f8f03
mikebru <32722658+mikebru@users.noreply.github.com>
2020-12-20 10:10:15 -0600

    added in page routing, put components into folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import HelloWorld from './HelloWorld';
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+
 import ResponsiveEmbed from 'react-responsive-embed'
 import { Connector } from 'react-mqtt';
 import {subscribe} from 'react-mqtt';
-import _MessageContainer from './MessageContainer.js';
+import _MessageContainer from './components/MessageContainer.js';
 
 const MessageContainer = subscribe({topic: 'pixelGarden'})(_MessageContainer);
 
 class App extends Component {
   render() {
     return (
-      <Connector mqttProps="wss://test.mosquitto.org:8081">
-        <div className="App"> 
-          <HelloWorld name="jim"/> 
+      <BrowserRouter>
+        <Connector mqttProps="wss://test.mosquitto.org:8081">
+          <div className="App"> 
 
-          <ResponsiveEmbed src='https://obs.ninja/?view=mikebru' allowFullScreen ratio='16:9'/>
+            <Navigation />
+              <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route component={Error}/>
+              </Switch>
 
-          <MessageContainer/>
-          <br/> <br/> <br/>
-        </div>
-      </Connector>
+
+            <ResponsiveEmbed src='https://obs.ninja/?view=mikebru' allowFullScreen ratio='16:9'/>
+            <MessageContainer/>
+          </div>
+        </Connector>
+      </BrowserRouter>
       );
     }
 }
